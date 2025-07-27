50e5665cf8571c21ad2a9475004d98834f80285c
Kristian G Hammond <77787608+Earth2Kristian@users.noreply.github.com>
2025-07-27 21:33:32 +0100

    Fixed the non-moving menu screen issue when player select back to menu after the battle.

--- Assets/MenuScreenManager.cs
+++ Assets/MenuScreenManager.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class MenuScreenManager : MonoBehaviour
 {
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
     public void LoadBattleScene()
     {
         SceneManager.LoadScene(1); 
